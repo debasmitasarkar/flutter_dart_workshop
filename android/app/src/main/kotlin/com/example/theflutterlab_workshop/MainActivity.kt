bb8420715674783e696189935745040a5f6f1573
Debasmita Sarkar <debasmitasarkar@Debasmitas-MacBook-Pro.local>
2023-09-24 17:38:19 +0530

    initial app

---
+++ android/app/src/main/kotlin/com/example/theflutterlab_workshop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.theflutterlab_workshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MoonApp());
+}
+
+class MoonApp extends StatelessWidget {
+  const MoonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Moon App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Moon App'),
+        ),
+        body: const Center(
+          child: Text('Hello Moon!\n- From Earth', style: TextStyle(fontSize: 30),),
+        ),
+      ),
+    );
+  }
+}
