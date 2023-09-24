a7f2b855ef3f41dfb700758bbc205ba3389a2a0b
Debasmita Sarkar <debasmitasarkar@Debasmitas-MacBook-Pro.local>
2023-09-24 20:18:29 +0530

    add image and text to initial app

---
+++ lib/texts.dart
@@ -0,0 +1,7 @@
+// Home Screen
+String appBarTitle = 'The Moon App';
+String moonTitle = "India's Moon Mission: Chandrayaan-3";
+String moonDescription =
+    "Chandrayaan-3, India's ambitious lunar exploration mission, aims to further our understanding of the moon. With the Vikram lander and Pragyan rover, ISRO continues its quest to unravel the mysteries of our closest celestial neighbor.";
+String buttonText = "Vikram Lander Details";
+String bottomNavbarText = "Developed with 💙 in Flutter";
